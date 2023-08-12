5dac6f1db008c0cf97f9c83c3940798925ae8897
ppabis <piotr@pabis.eu>
2023-08-12 17:15:28 +0200

    more configurable things

--- src/config.py
+++ src/config.py
@@ -1,1 +1,6 @@
-KEY="pc-api-abcdefghi.json"
+# Key you downloaded for service account
+KEY = "pc-api-12345678-12345678-abcdef.json"
+# Uri of the bucket from Google Play Console
+BUCKET_URI = "gs://pubsite_prod_rev_01234567891011121314/stats/installs/"
+# List of apps to query
+APPS = ["com.mycompany.myapp", "com.mycompany.myotherapp"]
